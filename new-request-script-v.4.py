4215045020eed354846fd0a3284bc8477ab9b361
Mr_joep <bartvdburgt03@gmail.com>
2023-12-14 00:20:00 +0100

    max_concurrent_requests fix

--- new-request-script-v.4.py
+++ new-request-script-v.4.py
@@ -49,4 +49,4 @@ if __name__ == "__main__":
     asyncio.run(check_website_once(website_url))
 
     # Step 2 and 3: Monitor the website indefinitely
-    asyncio.run(monitor_website(website_url, max_concurrent_requests=1000))
+    asyncio.run(monitor_website(website_url, max_concurrent_requests=100))
